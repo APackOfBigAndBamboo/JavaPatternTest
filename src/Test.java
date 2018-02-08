a67cdab2499c305af72f219b92a17112a45e9ef4
Super <1084150741@qq.com>
2018-02-08 15:11:54 +0800

    aa

--- src/Test.java
+++ src/Test.java
@@ -1,29 +1,17 @@
-import DecoratorPattern.Circle;
-
-
 
 public class Test {
     public static void main(String[] args) {
-        String xmbhcbzxcwy ="huangdawei-ss  ".trim();
+        String xmbhcbzxcwy = (null+"").trim();
+        String a = "aa"+"";
+        System.out.println(a.length());
+        System.out.println(xmbhcbzxcwy.length());
+        if (xmbhcbzxcwy!=null&&xmbhcbzxcwy.length() > 3) {
 
-        StringBuffer  buffer=new StringBuffer("hello");
-        System.out.println(buffer.reverse());
-        String temp=xmbhcbzxcwy .substring(xmbhcbzxcwy .length()-3,xmbhcbzxcwy .length());
-        if ("-SS".equalsIgnoreCase(temp));
-        {
-            xmbhcbzxcwy =xmbhcbzxcwy .substring(0,xmbhcbzxcwy .length()-3);
+            String temp = xmbhcbzxcwy.substring(xmbhcbzxcwy.length() - 3, xmbhcbzxcwy.length());
+            if ("-SS".equalsIgnoreCase(temp)) {
+                xmbhcbzxcwy = xmbhcbzxcwy.substring(0, xmbhcbzxcwy.length() - 3);
+            }
         }
-//        if ("-SQ".equals(temp)||"-SS".equals(temp)||"-ss".equals(temp)||"-sq".equals(temp)){
-//            xmbhcbzxcwy =xmbhcbzxcwy .substring(0,xmbhcbzxcwy .length()-3);
-//        }
-        System.out.println(xmbhcbzxcwy );
-        Circle a=new Circle();
-        Circle b=new Circle();
-
-        System.out.println(a);
-        System.out.println(b);
-        System.out.println(a.equals(b));
-        System.out.println(a==b);
-
+        System.out.println(xmbhcbzxcwy);
     }
 }
